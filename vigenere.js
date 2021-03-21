f8d541d003647717370c26f236f036c4a9bcb93f
FantasticFiasco <mattias.kindborg@gmail.com>
2021-03-21 11:50:20 +0100

    feat: vigenere decrypt

--- vigenere.js
+++ vigenere.js
@@ -3,6 +3,7 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 function usage() {
     console.log('Usage:')
     console.log('       node vigenere.js encrypt <key> <plain text>')
+    console.log('       node vigenere.js decrypt <key> <cipher text>')
     process.exit(1)
 }
 
@@ -44,6 +45,29 @@ function encrypt(key, plainText) {
     console.log(cipherText)
 }
 
+function decrypt(key, cipherText) {
+    key = key.toLowerCase()
+    let keyIndex = 0
+    let plainText = ''
+
+    for (let char of cipherText) {
+        const seed = alphabet.indexOf(key[keyIndex % key.length])
+        const caesar = createCaesar(seed)
+
+        const i = caesar.indexOf(char.toLowerCase())
+        if (i == -1) {
+            plainText += char
+            continue
+        }
+
+        const plainChar = alphabet[i]
+        plainText += isLowerCase(char) ? plainChar : plainChar.toUpperCase()
+        keyIndex++
+    }
+
+    console.log(plainText)
+}
+
 let args = process.argv.slice(2)
 if (args.length < 1) {
     usage()
@@ -58,6 +82,12 @@ switch (verb) {
             usage()
         }
         encrypt(...args)
+        break
+    case 'decrypt':
+        if (args.length != 2) {
+            usage()
+        }
+        decrypt(...args)
         break
     default:
         usage()
