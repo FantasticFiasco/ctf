d2e27003eb921c0a36b7e860a71f6e20553f4c96
FantasticFiasco <mattias@kindb.org>
2021-04-04 09:15:31 +0200

    wip

--- some-assembly-required-2/clean.js
+++ some-assembly-required-2/clean.js
@@ -1,55 +1,22 @@
 const fetch = require('node-fetch')
 
-const strings = [
-    '615361geljRK',
-    'copy_char',
-    'value',
-    '207aLjBod',
-    '1301420SaUSqf',
-    '233ZRpipt',
-    '2224QffgXU',
-    'check_flag',
-    '408533hsoVYx',
-    'instance',
-    '278338GVFUrH',
-    'Correct!',
-    '549933ZVjkwI',
-    'innerHTML',
-    'charCodeAt',
-    './aD8SvhyVkb',
-    'result',
-    '977AzKzwq',
-    'Incorrect!',
-    'myExports',
-    'length',
-    'getElementById',
-    '1jIrMBu',
-    'input',
-]
+const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-const getString = function (index) {
-    index = index - 0xc3
-    let string = strings[index]
-    return string
-}
-
-let myExports
 ;(async () => {
-    const _0x835967 = getString
-    let _0x1adb5f = await fetch('http://mercury.picoctf.net:61778/' + _0x835967(0xd2)),
-        _0x355961 = await WebAssembly['instantiate'](await _0x1adb5f['arrayBuffer']()),
-        _0x5c0ffa = _0x355961[_0x835967(0xcc)]
-    myExports = _0x5c0ffa[_0x835967(0xd6)]
-})()
+    const response = await fetch('http://mercury.picoctf.net:61778/aD8SvhyVkb')
+    const arrayBuffer = await response.arrayBuffer()
+    const webAssembly = await WebAssembly.instantiate(arrayBuffer)
+    const webAssemblyExports = webAssembly.instance.exports
 
-function onButtonPress() {
-    const _0x50ea62 = getString
-    let _0x5f4170 = document[_0x50ea62(0xd8)](_0x50ea62(0xda))[_0x50ea62(0xc5)]
-    for (let _0x19d3ca = 0x0; _0x19d3ca < _0x5f4170['length']; _0x19d3ca++) {
-        myExports[_0x50ea62(0xc4)](_0x5f4170[_0x50ea62(0xd1)](_0x19d3ca), _0x19d3ca)
+    console.log('web assembly exports')
+    console.log(webAssemblyExports)
+
+    let password = 'p'
+
+    for (let i = 0; i < password.length; i++) {
+        webAssemblyExports.copy_char(password.charCodeAt(i), i)
     }
-    myExports['copy_char'](0x0, _0x5f4170[_0x50ea62(0xd7)]),
-        myExports[_0x50ea62(0xca)]() == 0x1
-            ? (document['getElementById'](_0x50ea62(0xd3))[_0x50ea62(0xd0)] = _0x50ea62(0xce))
-            : (document[_0x50ea62(0xd8)](_0x50ea62(0xd3))['innerHTML'] = _0x50ea62(0xd5))
-}
+
+    webAssemblyExports.copy_char(0, password.length)
+    webAssemblyExports.check_flag() == 1 ? console.log('Correct!') : console.log('Incorrect!')
+})()
